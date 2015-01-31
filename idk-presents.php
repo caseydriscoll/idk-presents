ea4100905bdedca43b6fbe7a29319bdee1720e6d
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2015-01-31 10:26:44 -0500

    Init commit

---
+++ idk-presents.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Plugin Name: iDK Presents
+ * Description: A Presentation Plugin
+ * Author: Casey Patrick Driscoll
+ * Author URI: http://caseypatrickdriscoll.com
+ *
+ */
+
+class IDK_Presents {
+
+
+	function __construct() {
+		
+	}
+
+
+}
+
+new IDK_Presents();
